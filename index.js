7161869444c0f94376163416c7433c7720223e8e
ayarmola <andrii.yarmola@gmail.com>
2017-07-14 19:00:47 +0300

    users

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import fs from 'fs';
+import _ from 'lodash';
 
-// import users from './routes/users';
+let users = [];
+
+fs.readFile('users.json', {encoding: 'utf8'}, (err, data) => {
+  if (err) throw err;
+
+  JSON.parse(data).forEach(
+    (user) => {
+      user.name = _.startCase(user.name);
+      users.push(user);
+    }
+  )
+})
 
 let app = express();
 
 app.use(bodyParser.json());
 
-// app.use('/api/users', users);
-
 app.get('/', (req, res) => {
-  res.send('Hell so1');
+  res.send(JSON.stringify(users, null, 2));
 });
-
 
 app.listen(3000, () => console.log('Running on localhost:3000'));
