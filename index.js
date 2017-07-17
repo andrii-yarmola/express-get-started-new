13d42a65216d1e6f979c96299dd86dbfe96019e9
ayarmola <andrii.yarmola@gmail.com>
2017-07-17 18:31:34 +0300

    views

--- index.js
+++ index.js
@@ -18,10 +18,19 @@ fs.readFile('users.json', {encoding: 'utf8'}, (err, data) => {
 
 let app = express();
 
+app.set('views', './views');
+app.set('view engine', 'jade');
+
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.send(JSON.stringify(users, null, 2));
+  // res.send(JSON.stringify(users, null, 2));
+  res.render('index', {users: users})
 });
+
+app.get('/:username', (req, res) => {
+  const username = req.params.username;
+  res.send(username);
+}); 
 
 app.listen(3000, () => console.log('Running on localhost:3000'));
